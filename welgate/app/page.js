72add16a722ae5fe50e61c62658f7e9076991593
gopika gopu <gopikalgopal@gmail.com>
2025-05-05 17:53:15 +0530

    header imported

--- welgate/app/page.js
+++ welgate/app/page.js
@@ -1,5 +1,5 @@
 import Footer from '@/components/common/Footer'
-import Header from '@/components/common/header'
+import Header from '@/components/common/Header'
 import React from 'react'
 
 function page() {
