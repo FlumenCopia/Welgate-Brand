c5dcac46f55f6a8b98e5c19edcd1a73e70cfc6c8
Arathiss <arathiss827@gmail.com>
2025-05-06 12:55:19 +0530

    feature cards

--- welgate/components/Home/Banner.js
+++ welgate/components/Home/Banner.js
@@ -8,13 +8,13 @@ function Banner() {
                     <div className="row align-items-center justify-content-center">
                         <div className="col-xl-6 col-lg-6 order-0 order-lg-2">
                             <div className="tg-banner-img text-center wow fadeInRight" data-wow-delay=".6s">
-                                <img src="assets/img/banner/03.png" alt="img"/>
+                                <img src="https://welgatelifestyle.com/assets/images/thumbs/03.png" alt="img"/>
                             </div>
                         </div>
                         <div className="col-xl-6 col-lg-6 col-md-10">
                             <div className="tg-banner-content">
                                 <h4 className="sub-title wow fadeInUp" data-wow-delay=".2s">Pure Wellness!</h4>
-                                <h2 className="title wow fadeInUp" data-wow-delay=".4s"> Healthier  Tomorrow <br/>  Starts Today</h2>
+                                <h2 className="title wow fadeInUp" data-wow-delay=".4s"> Healthier  Tomorrow Starts Today</h2>
                                 <p className=" wow fadeInUp" data-wow-delay=".6s">Discover simple ways to live healthier—every single day.</p>
                                 <div className="tg-banner-btn wow fadeInUp" data-wow-delay=".8s">
                                     <a href="shop-details.html" className="tg-btn">purchase now</a>
