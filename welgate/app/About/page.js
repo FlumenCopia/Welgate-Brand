1d27015ab9470f7a3b74aea59bbc69395af2c9a4
gopika gopu <gopikalgopal@gmail.com>
2025-05-06 11:38:10 +0530

    about sec and  more page

---
+++ welgate/app/About/page.js
@@ -0,0 +1,18 @@
+
+
+import Footer from '@/components/common/Footer'
+import Header from '@/components/common/Header'
+import Feature2 from '@/components/Home/Feature2'
+import VideoArea from '@/components/Home/VideoArea'
+import React from 'react'
+
+export const page = () => {
+  return (
+    <div>
+         <Header/>
+        <VideoArea/>
+        <Footer/>
+    </div>
+  )
+}
+export default page
