c933bae4446aaa3c28d71b440b7d22bdcbc9eabf
gopika gopu <gopikalgopal@gmail.com>
2025-05-08 12:49:09 +0530

    about page changes

--- welgate/components/About-us/About_value.js
+++ welgate/components/About-us/About_value.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About_value = () => {
   return (
     <div>
-         <div class="features-products-wrap">
+         <div class="features-products-wrap ">
                         <div class="row justify-content-center">
                             <div class="col-lg-6 col-md-8">
                                 <div class="features-products-thumb">
@@ -15,13 +15,13 @@ const About_value = () => {
                             </div>
                             <div class="col-lg-6 col-md-10">
                                 <div class="features-product-content">
-                                    <h2 class="title"><a href="#">Why Choose Us</a></h2>
+                                    <h2 class="title">Why Choose Us</h2>
                                     {/* <h6 class="features-product-quantity">High-strength, 4000IU</h6> */}
-                                    <p> - Legacy of Trust: Decades of experience rooted in passion and innovation.</p>
-                                    <p> - Entertainment to Organic Living: Evolved from bringing joy through music and movies to promoting wellness through organic food.</p>
-                                    <p> - Purity & Sustainability: Committed to chemical-free, ethical, and sustainable farming practices. </p>
-                                    <p> - Future-Ready: Led by the next generation with a global vision for healthier living.</p>
-                                    <p> - More Than a Brand: A movement built on care, quality, and meaningful transformation.</p>
+                                    <p> <span className='color-wrap'>- Legacy of Trust: </span>Decades of experience rooted in passion and innovation.</p>
+                                    <p> <span className='color-wrap'>- Entertainment to Organic Living:</span> Evolved from bringing joy through music and movies to promoting wellness through organic food.</p>
+                                    <p> <span className='color-wrap'>- Purity & Sustainability:</span> Committed to chemical-free, ethical, and sustainable farming practices. </p>
+                                    <p> <span className='color-wrap'>- Future-Ready:</span> Led by the next generation with a global vision for healthier living.</p>
+                                    <p><span className='color-wrap'> - More Than a Brand:</span> A movement built on care, quality, and meaningful transformation.</p>
                                     {/* <div class="features-product-bottom">
                                         <a href="shop-details.html" class="btn">Shop Now</a>
                                         <span class="price">$59.99 <span class="old-price">$79.99</span></span>
