49b7a38b3a67b7f705aa99d80cb790fa43b8a45f
Arathiss <arathiss827@gmail.com>
2025-05-08 10:17:46 +0530

    header and footer is added in gallery section

--- welgate/app/Gallery/page.js
+++ welgate/app/Gallery/page.js
@@ -1,10 +1,14 @@
-import Gallerypage from '@/components/galleries/gallerypage'
+import Footer from '@/components/common/Footer'
+import Header from '@/components/common/Header'
+import Gallerypage from '@/components/galleries/Gallerypage'
 import React from 'react'
 
 function page() {
   return (
     <div>
+        <Header/>
         <Gallerypage/>
+        <Footer/>
     </div>
   )
 }
