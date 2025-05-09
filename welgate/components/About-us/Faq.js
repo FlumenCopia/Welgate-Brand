2b4cd5bc79a6517c6d7cf79969f2a1978e24c49e
Aromalvg <aromalvijayan448@gmail.com>
2025-05-09 10:22:36 +0530

    blog resp

--- welgate/components/About-us/Faq.js
+++ welgate/components/About-us/Faq.js
@@ -5,7 +5,7 @@ function Faq() {
     <div>
          <section className="faq-area position-relative">
                 <div className="row justify-content-end g-0">
-                    <div className="faq-img" data-background="assets/img/others/faq_img.jpg">
+                    <div className="faq-img" data-background="https://img.freepik.com/premium-photo/shopping-cart-wooden-blocks_740368-1716.jpg?w=1380">
                         {/* <a href="" className=""><i className=""></i></a> */}
                     </div>
                     <div className="col-lg-6">
