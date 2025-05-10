75a4e4316caa58f113ca4cc460be055fa3426828
Aromalvg <aromalvijayan448@gmail.com>
2025-05-10 11:01:11 +0530

    header and scroll js error fix

--- welgate/components/Scroll/Scrolltop.js
+++ welgate/components/Scroll/Scrolltop.js
@@ -1,20 +1,8 @@
 'use client'
 import React from 'react'
-import { useEffect } from 'react';
 
 function Scrolltop() {
 
-  useEffect(() => {
-    // Load jQuery and custom scroll function
-    const jquery = document.createElement("script");
-    jquery.src = "/assets/js/vendor/jquery-3.6.0.min.js";
-    jquery.onload = () => {
-      const main = document.createElement("script");
-      main.src = "/assets/js/main.js";
-      document.body.appendChild(main);
-    };
-    document.body.appendChild(jquery);
-  }, []);
 
   return (
     <div>
