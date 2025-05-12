e885d100fb006c279a197ec2fa89c350598d077e
Aromalvg <aromalvijayan448@gmail.com>
2025-05-12 17:46:09 +0530

    history, about img

---
+++ welgate/components/Home/History2.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function History2() {
+  return (
+    <div>
+        <section class="formula-area2" >
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-5 order-0 order-lg-0">
+                            <div class="formula-img2">
+                                <img  src="/assets/img/WhatsApp Image 2025-05-12 at 14.39.11_b5a8f290.jpg" alt="img"  style={{width:'400px',borderRadius:'20px'}}/>
+                            </div>
+                        </div>
+                        <div class="col-lg-7">
+                            <div class="formula-content">
+                                <div class="section-title white-title mb-50">
+                                    {/* <p class="sub-title">.. Suxnix Formula ..</p> */}
+                                    <p class="title text-white">The Shift to Farm Fresh
+</p>
+                                </div>
+                                {/* <ul class="formula-list list-wrap">
+                                    <li>Tastes like dessert without added sugars or sugar alcohols</li>
+                                    <li>No artificial sweeteners, dairy, say or corn fiber</li>
+                                    <li>10G of collagen protein from grass-fed cows</li>
+                                    <li>Formulated to reduce blood sugar intact</li>
+                                    <li>Organic almond Butter, Sunflower Lecithin</li>
+                                    <li>No energy crashes. Collagen Protein, Stevia</li>
+                                    <li>10G of collagen protein from grass-fed cows</li>
+                                </ul> */}
+                                <p className='classp' style={{textAlign:'justify'}}>A thought sparked a movement. Understanding the importance of pure, chemical-free food, Welgate took a bold step toward organic farming."As Welgate thrived in the entertainment industry, a deeper realization took root the importance of pure, chemical-free food for a healthier life. Witnessing the growing concerns over food quality and its impact on well-being, the founders saw an opportunity to create real change.
+                                    With a commitment to sustainability and wellness, Welgate took a bold step into organic farming, ensuring that every product nurtured the body while preserving the earth. What started as an idea soon became a mission to redefine the way people eat naturally, ethically, and with care.
+                                </p>
+                                {/* <a href="contact.html" class="btn btn-two">Know More</a> */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    </div>
+  )
+}
+
+export default History2
