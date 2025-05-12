01b721446e06c74b923900c3f604454a6d5ec1a8
Arathiss <arathiss827@gmail.com>
2025-05-12 11:24:56 +0530

    history

---
+++ welgate/components/Home/History.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function History() {
+  return (
+    <div>
+             <section class="formula-area formula-bg" >
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6 order-0 order-lg-2">
+                            <div class="formula-img">
+                                <img src="assets/img/WhatsApp Image 3.jpg" alt="img"  style={{width:'300px'}}/>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="formula-content">
+                                <div class="section-title white-title mb-50">
+                                    {/* <p class="sub-title">.. Suxnix Formula ..</p> */}
+                                    <p class="title">The Birth of Our Entertainment Journey</p>
+                                </div>
+                                {/* <ul class="formula-list list-wrap">
+                                    <li>Tastes like dessert without added sugars or sugar alcohols</li>
+                                    <li>No artificial sweeteners, dairy, say or corn fiber</li>
+                                    <li>10G of collagen protein from grass-fed cows</li>
+                                    <li>Formulated to reduce blood sugar intact</li>
+                                    <li>Organic almond Butter, Sunflower Lecithin</li>
+                                    <li>No energy crashes. Collagen Protein, Stevia</li>
+                                    <li>10G of collagen protein from grass-fed cows</li>
+                                </ul> */}
+                                <p className='classp' style={{fontSize:'15px',textAlign:'justify'}}>"From a single idea to a thriving business-Welgate opened its first CD stores, making music and movies accessible to everyone."With a passion for bringing entertainment to the masses, Welgate took its first big leap-opening CD stores that made music and movies easily accessible to everyone. These stores quickly became a hub for people seeking the latest hits, classic collections, and rare finds. It was more than just a business; it was about creating experiences, connecting people with stories, and making entertainment a part of everyday life. This success laid the foundation for something even greater, as Welgate's journey was only getting started."</p>
+                                {/* <a href="contact.html" class="btn btn-two">Know More</a> */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    </div>
+  )
+}
+
+export default History
