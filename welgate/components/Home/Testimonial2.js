a462d7d93765b6b95eda3414bc89cffc3e65c5ab
Aromalvg <aromalvijayan448@gmail.com>
2025-05-13 11:20:46 +0530

    testimonial react slick

--- welgate/components/Home/Testimonial2.js
+++ welgate/components/Home/Testimonial2.js
@@ -1,157 +1,114 @@
-import React from 'react'
+'use client'
+import React, { useRef } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-function Testimonial2() {
+function PrevArrow({ onClick }) {
   return (
-    <div>
-        <section className="testimonial-area testimonial-bg-two section-pt-130 section-pb-130">
-                <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-xl-6 col-lg-8">
-                            <div className="section-title section-title-two text-center  mb-70">
-                                <span className="sub-title">
-                                    <img src="assets/img/icons/sub_left.svg" className="injectable" alt="img"/>
-                                    <p className='subtitle-colour'>Testimonial</p>
-                                    <img src="assets/img/icons/sub_right.svg" className="injectable" alt="img"/>
-                                </span>
-                                <p className="title pfont2">Clients Feedback</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row testimonial-active-two">
-                        <div className="col-lg-6">
-                            <div className="testimonial-item-two">
-                                <div className="testimonial-top">
-                                    <div className="testi-avatar">
-                                        <div className="thumb">
-                                            <img src="assets/img/others/testi_avatar01.jpg" alt="img"/>
-                                        </div>
-                                        <div className="info">
-                                            <h6 className="name">Darrell Steward</h6>
-                                            <span className="designation">Founder of (Rirax)</span>
-                                        </div>
-                                    </div>
-                                    <div className="testi-rating">
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                    </div>
-                                </div>
-                                <div className="testimonial-content-two">
-                                    <p>Our product is only a tool. It will take you wherever you wish, but it will not replace you as the driver. It will take you wherever you wish, but it will not replace you as the driver.Collax is only a tool. It will take you wherever you wish, but it will</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6">
-                            <div className="testimonial-item-two">
-                                <div className="testimonial-top">
-                                    <div className="testi-avatar">
-                                        <div className="thumb">
-                                            <img src="assets/img/others/testi_avatar02.jpg" alt="img"/>
-                                        </div>
-                                        <div className="info">
-                                            <h6 className="name">Darrell Steward</h6>
-                                            <span className="designation">Founder of (Rirax)</span>
-                                        </div>
-                                    </div>
-                                    <div className="testi-rating">
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                    </div>
-                                </div>
-                                <div className="testimonial-content-two">
-                                    <p>Our product is only a tool. It will take you wherever you wish, but it will not replace you as the driver. It will take you wherever you wish, but it will not replace you as the driver.Collax is only a tool. It will take you wherever you wish, but it will</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6">
-                            <div className="testimonial-item-two">
-                                <div className="testimonial-top">
-                                    <div className="testi-avatar">
-                                        <div className="thumb">
-                                            <img src="assets/img/others/testi_avatar03.jpg" alt="img"/>
-                                        </div>
-                                        <div className="info">
-                                            <h6 className="name">Darrell Steward</h6>
-                                            <span className="designation">Founder of (Rirax)</span>
-                                        </div>
-                                    </div>
-                                    <div className="testi-rating">
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                    </div>
-                                </div>
-                                <div className="testimonial-content-two">
-                                    <p>Our product is only a tool. It will take you wherever you wish, but it will not replace you as the driver. It will take you wherever you wish, but it will not replace you as the driver.Collax is only a tool. It will take you wherever you wish, but it will</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6">
-                            <div className="testimonial-item-two">
-                                <div className="testimonial-top">
-                                    <div className="testi-avatar">
-                                        <div className="thumb">
-                                            <img src="assets/img/others/testi_avatar01.jpg" alt="img"/>
-                                        </div>
-                                        <div className="info">
-                                            <h6 className="name">Darrell Steward</h6>
-                                            <span className="designation">Founder of (Rirax)</span>
-                                        </div>
-                                    </div>
-                                    <div className="testi-rating">
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                    </div>
-                                </div>
-                                <div className="testimonial-content-two">
-                                    <p>Our product is only a tool. It will take you wherever you wish, but it will not replace you as the driver. It will take you wherever you wish, but it will not replace you as the driver.Collax is only a tool. It will take you wherever you wish, but it will</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6">
-                            <div className="testimonial-item-two">
-                                <div className="testimonial-top">
-                                    <div className="testi-avatar">
-                                        <div className="thumb">
-                                            <img src="assets/img/others/testi_avatar01.jpg" alt="img"/>
-                                        </div>
-                                        <div className="info">
-                                            <h6 className="name">Darrell Steward</h6>
-                                            <span className="designation">Founder of (Rirax)</span>
-                                        </div>
-                                    </div>
-                                    <div className="testi-rating">
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                        <i className="fas fa-star"></i>
-                                    </div>
-                                </div>
-                                <div className="testimonial-content-two">
-                                    <p>Our product is only a tool. It will take you wherever you wish, but it will not replace you as the driver. It will take you wherever you wish, but it will not replace you as the driver.Collax is only a tool. It will take you wherever you wish, but it will</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="testimonial-nav">
-                        <a className="prev-arrow"><i className="flaticon-left-arrow"></i></a>
-                        <a className="next-arrow"><i className="flaticon-right-arrow"></i></a>
-                    </div>
-                </div>
-            </section>
-    </div>
-  )
+    <a className="prev-arrow slick-arrow" onClick={onClick}>
+      <i className="flaticon-left-arrow"></i>
+    </a>
+  );
 }
 
-export default Testimonial2
+function NextArrow({ onClick }) {
+  return (
+    <a className="next-arrow slick-arrow" onClick={onClick}>
+      <i className="flaticon-right-arrow"></i>
+    </a>
+  );
+}
+
+function Testimonial2() {
+  const sliderRef = useRef(null); // 🔁 use ref for manual control
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    autoplay: true,
+    arrows: false, // ❌ DISABLE built-in arrows
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <section className="testimonial-area testimonial-bg-two section-pt-130 section-pb-130">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-xl-6 col-lg-8">
+            <div className="section-title section-title-two text-center mb-70">
+              <span className="sub-title">
+                <img src="assets/img/icons/sub_left.svg" className="injectable" alt="img" />
+                <p className="subtitle-colour">Testimonial</p>
+                <img src="assets/img/icons/sub_right.svg" className="injectable" alt="img" />
+              </span>
+              <p className="title pfont2">Clients Feedback</p>
+            </div>
+          </div>
+        </div>
+
+        {/* 🔁 Slider with ref */}
+        <Slider ref={sliderRef} {...settings} className="testimonial-active-two row">
+          {[...Array(5)].map((_, index) => (
+            <div key={index} className="col-lg-6">
+              <div className="testimonial-item-two">
+                <div className="testimonial-top">
+                  <div className="testi-avatar">
+                    {/* <div className="thumb"><img src="assets/img/others/testi_avatar01.jpg" alt="img" /></div> */}
+                    <div className="info">
+                      <h6 className="name">Darrell Steward</h6>
+                      <span className="designation">Founder of (Rirax)</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="testimonial-content-two">
+                  <p>
+                    Our product is only a tool. It will take you wherever you wish, but it will not
+                    replace you as the driver. Collax is only a tool. It will take you wherever you
+                    wish, but it will....
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
+
+        {/* ✅ Custom arrows BELOW the slider */}
+        <div className="testimonial-nav">
+          <PrevArrow onClick={() => sliderRef.current?.slickPrev()} />
+          <NextArrow onClick={() => sliderRef.current?.slickNext()} />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Testimonial2;
