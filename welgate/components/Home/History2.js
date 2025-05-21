a0b54eaf459d0cdc4fda0bc42e7c7f03cd6551fd
Aromalvg <aromalvijayan448@gmail.com>
2025-05-21 12:16:08 +0530

    history

--- welgate/components/Home/History2.js
+++ welgate/components/Home/History2.js
@@ -2,27 +2,16 @@ import React from 'react';
 
 function History2() {
   return (
-    <div className="container d-flex justify-content-center mt-5">
-  <div className="row w-100 justify-content-center text-center">
-
-    <div className="col-6 d-flex justify-content-center">
-      <img
-        src="/assets/img/WhatsApp Image 1.jpg"
-        alt=""
-        style={{ width: '50%', objectFit: 'cover',borderRadius:'20px' }}
-      />
-    </div>
-
-    <div className="col-6 d-flex justify-content-center">
-      <img
-        src="/assets/img/WhatsApp Image 2025-05-12 at 14.39.11_b5a8f290.jpg"
-        alt=""
-        style={{ width: '50%', objectFit: 'cover',borderRadius:'20px' }}
-      />
-    </div>
-
-  </div>
+ <div class="flex-gallery">
+  <div class="flex-item tall"><img src="/assets/img/WhatsApp Image 2025-05-12 at 15.37.09_045f681b.jpg" alt="Image 1" /></div>
+  <div class="flex-item wide"><img src="/assets/img/WhatsApp Image 3.jpg" alt="Image 2" /></div>
+  <div class="flex-item square"><img src="/assets/img/WhatsApp Image 2.jpg" alt="Image 3" /></div>
+  <div class="flex-item small"><img src="/assets/img/WhatsApp Image 1.jpg" alt="Image 4" /></div>
+  <div class="flex-item medium"><img src="/assets/img/WhatsApp.jpg" alt="Image 5" /></div>
+  <div class="flex-item tall"><img src="/assets/img/WhatsApp Image 2025-05-12 at 14.39.11_b5a8f290.jpg" alt="Image 6" /></div>
 </div>
+
+
 
   );
 }
