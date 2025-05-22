93ec1c7caa8b116bbf89d8e819bbbfd3c7e315b3
Aromalvg <aromalvijayan448@gmail.com>
2025-05-22 14:22:34 +0530

    feature img

--- welgate/components/Home/Feature2.js
+++ welgate/components/Home/Feature2.js
@@ -12,7 +12,7 @@ function Feature2() {
                         <div className="row justify-content-center">
                             <div className="col-lg-6 col-md-8 order-0 order-lg-2 bothimg">
                                 <div className="tg-supplement-img text-end  wow fadeInRight" data-wow-delay=".2s">
-                                    <img className='imgsize' src="/assets/img/2783001_15762-removebg-preview.png" alt=""style={{ transform: 'scaleX(-1)' }}/ >
+                                    <img className='imgsize' src="/assets/img/icons/ChatGPT Image May 22, 2025, 02_12_58 PM.png" alt=""/ >
                                 </div>
                                 <div className="tg-supplement-shape">
                                     <img src="assets/img/others/supplement_shape02.png" alt="" className="rotateme"/>
